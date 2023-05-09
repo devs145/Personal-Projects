d536820aa662ba915f939c1d007e63badb85300d
sahil devnani <sahil.devnani@uleth.ca>
2023-05-09 11:46:17 -0600

    commit

---
+++ CALCULATOR/my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import App from './App';
+import DigitButton from './DigitButton';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
